4589af889755159b9ff88178d586ad62aaa01669
Tanner Schmutte <tannerschmutte@gmail.com>
2021-02-05 13:42:39 -0800

    infoWindows onMouseOver map markers

--- react-app/src/components/Home/Map/index.js
+++ react-app/src/components/Home/Map/index.js
@@ -3,6 +3,7 @@ import GoogleMapReact from 'google-map-react';
 import styled from 'styled-components';
 import { useHistory } from 'react-router-dom';
 // import { useSearchLocation } from "../../SearchContext";
+import Marker from '../Marker';
 
 const ContainerContainer = styled.div`
     display: flex;
@@ -10,7 +11,6 @@ const ContainerContainer = styled.div`
     justify-content: center;
     // background-color: #fff6dd;
     background-color: #bbe6c0;
-
 `;
 
 const MapContainer = styled.div`
@@ -22,20 +22,9 @@ const MapContainer = styled.div`
     position: relative;
 `;
 
-const PinContainer = styled.div`
-    background-color: #21b786;
-    color: black;
-    width: 10px;
-    height: 10px;
-    border-radius: 100%;
-    border: 3px solid black;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-`;
-
 export default function Map({ courts }) {
     const history = useHistory();
+
     return (
         <ContainerContainer>
             <MapContainer>
@@ -48,159 +37,110 @@ export default function Map({ courts }) {
                 >
                     {/* {courts &&
                         courts.map((court) => (
-                            <PinContainer
+                            <Marker
                                 onClick={(e) =>
                                     history.push(`/courts/${court.id}`)
                                 }
                                 key={court.id}
                                 lat={court.latitude}
                                 lng={court.longitude}
-                            ></PinContainer>
+                            ></Marker>
                         ))} */}
-                    <PinContainer
-                        lat={37.761}
-                        lng={-122.4272}
-                        text="Dolores Park"
-                    />
-                    <PinContainer
+                    <Marker lat={37.761} lng={-122.4272} name="Dolores Park" />
+                    <Marker
                         lat={37.7702}
                         lng={-122.459}
-                        text="Golden Gate Park"
+                        name="Golden Gate Park"
                     />
-                    <PinContainer
+                    <Marker
                         lat={37.79265}
                         lng={-122.42617}
-                        text="Lafayette Park"
+                        name="Lafayette Park"
                     />
-                    <PinContainer
-                        lat={37.792260}
-                        lng={-122.436250}
-                        text="Alta Plaza"
+                    <Marker lat={37.79226} lng={-122.43625} name="Alta Plaza" />
+                    <Marker lat={37.73611} lng={-122.43978} name="Glen Park" />
+                    <Marker
+                        lat={37.7428}
+                        lng={-122.48042}
+                        name="McCoppin Park"
                     />
-                    <PinContainer
-                        lat={37.736110}
-                        lng={-122.439780}
-                        text="Glen Park"
+                    <Marker
+                        lat={37.78269}
+                        lng={-122.49083}
+                        name="Margaret Osborne DuPont Park"
                     />
-                    <PinContainer
-                        lat={37.742800}
-                        lng={-122.480420}
-                        text="McCoppin Park"
-                    />
-                    <PinContainer
-                        lat={37.782690}
-                        lng={-122.490830}
-                        text="Margaret Osborne DuPont Park"
-                    />
-                    <PinContainer
+                    <Marker
                         lat={37.7653}
                         lng={-122.4386}
-                        text="Corona Heights Park"
+                        name="Corona Heights Park"
                     />
-                    <PinContainer
-                        lat={37.7512}
-                        lng={-122.4393}
-                        text="Noe Valley"
-                    />
-                    <PinContainer
+                    <Marker lat={37.7512} lng={-122.4393} name="Noe Valley" />
+                    <Marker
                         lat={37.7138207}
                         lng={-122.431916}
-                        text="Crocker Amazon Playground"
+                        name="Crocker Amazon Playground"
                     />
-                    <PinContainer
-                        lat={37.7253}
-                        lng={-122.4431}
-                        text="Balboa Park"
-                    />
-                    <PinContainer
+                    <Marker lat={37.7253} lng={-122.4431} name="Balboa Park" />
+                    <Marker
                         lat={37.7569}
-                        lng={-122.3970}
-                        text="Potrero Recreation Center"
+                        lng={-122.397}
+                        name="Potrero Recreation Center"
                     />
-                    <PinContainer
-                        lat={37.7764}
-                        lng={-122.4347}
-                        text="Alamo Square"
-                    />
-                    <PinContainer
+                    <Marker lat={37.7764} lng={-122.4347} name="Alamo Square" />
+                    <Marker
                         lat={37.7684}
                         lng={-122.4414}
-                        text="Buena Vista Park"
+                        name="Buena Vista Park"
                     />
-                    <PinContainer
+                    <Marker
                         lat={37.8021}
                         lng={-122.4317}
-                        text="Presidio YMCA Ruger"
+                        name="Presidio YMCA Ruger"
                     />
-                    <PinContainer
-                        lat={37.7971}
-                        lng={-122.4483}
-                        text="Moscone"
-                    />
-                    <PinContainer
+                    <Marker lat={37.7971} lng={-122.4483} name="Moscone" />
+                    <Marker
                         lat={37.80169}
                         lng={-122.420069}
-                        text="Alice Marble"
+                        name="Alice Marble"
                     />
-                    <PinContainer
+                    <Marker
                         lat={37.7846658}
                         lng={-122.435976}
-                        text="Hamilton"
+                        name="Hamilton"
                     />
-                    <PinContainer
-                        lat={37.725989}
-                        lng={-122.483363}
-                        text="SFSU"
-                    />
-                    <PinContainer
+                    <Marker lat={37.725989} lng={-122.483363} name="SFSU" />
+                    <Marker
                         lat={37.7504211}
                         lng={-122.4062595}
-                        text="James Rolph Jr Playground"
+                        name="James Rolph Jr Playground"
                     />
-                    <PinContainer
+                    <Marker
                         lat={37.7369294}
                         lng={-122.4204963}
-                        text="Holly Park"
+                        name="Holly Park"
                     />
-                    <PinContainer
+                    <Marker
                         lat={37.77942865}
                         lng={-122.45774677}
-                        text="Angelo J Rossi Playground"
+                        name="Angelo J Rossi Playground"
                     />
-                    <PinContainer
+                    <Marker
                         lat={37.763881524}
                         lng={-122.400189}
-                        text="Jackson Playground"
+                        name="Jackson Playground"
                     />
-                    <PinContainer
+                    <Marker
                         lat={37.73607}
                         lng={-122.47816}
-                        text="Frances M McAteer"
+                        name="Frances M McAteer"
                     />
-                    <PinContainer
+                    <Marker
                         lat={37.7568065}
                         lng={-122.487155329}
-                        text="Sunset Recreation"
+                        name="Sunset Recreation"
                     />
                 </GoogleMapReact>
             </MapContainer>
         </ContainerContainer>
     );
 }
-
-// export class MapContainer extends React.Component {
-//     state = {
-//         events: [
-//             {
-//                 latitude: 37.761,
-//                 longitude: -122.4272,
-//             },
-//             {
-//                 latitude: 37.7702,
-//                 longitude: 122.459,
-//             },
-//             {
-//                 latitude: 37.79265,
-//                 longitude: -122.42617,
-//             },
-//         ],
---
+++ react-app/src/components/Home/Marker/index.js
@@ -0,0 +1,56 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import { useLayer, Arrow } from 'react-laag';
+
+const StyledMarker = styled.div`
+    background-color: #21b786;
+    color: black;
+    width: 10px;
+    height: 10px;
+    border-radius: 100%;
+    border: 3px solid black;
+    cursor: pointer;
+`;
+
+const InfoBox = styled.div`
+    padding: 1em;
+    border-radius: 50%;
+    box-shadow: 1px 1px 10px 1px rgba(0, 0, 0, 0.2);
+    border: 3px solid black;
+    background-color: #21b786;
+    font-family: 'Optima';
+    font-weight: bold;
+`;
+
+export default function Marker({ name }) {
+    const [isOpen, setOpen] = useState(false);
+    const { triggerProps, layerProps, arrowProps, renderLayer } = useLayer({
+        isOpen,
+        triggerOffset: 10,
+        auto: true,
+        overflowContainer: false,
+        onOutsideClick: () => setOpen(false),
+        // container: 'infoboxlayer',
+    });
+    return (
+        <>
+            <StyledMarker
+                {...triggerProps}
+                onMouseOver={() => setOpen(true)}
+                onMouseLeave={() => setOpen(false)}
+            />
+            {isOpen &&
+                renderLayer(
+                    <InfoBox {...layerProps}>
+                        {name}
+                        <Arrow
+                            {...arrowProps}
+                            borderWidth={2}
+                            borderColor="black"
+                            backgroundColor="#21B786"
+                        />
+                    </InfoBox>
+                )}
+        </>
+    );
+}
