b18e2dbed899f4a531295be242ba03302a7b7fc5
Tanner Schmutte <tannerschmutte@gmail.com>
2021-03-10 17:06:46 -0800

    remove comment

--- react-app/src/components/Court/Header/index.js
+++ react-app/src/components/Court/Header/index.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-// import { useSelector } from 'react-redux';
 import { useParams, useHistory } from 'react-router-dom';
 
 import logo from '../../../media/black_logo.png';
