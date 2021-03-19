495b34210334e63be6ddf64f6cdf4e7993dc2f1e
Tanner Schmutte <tannerschmutte@gmail.com>
2021-03-19 13:20:50 -0700

    add react DatePicker

--- react-app/src/components/Profile/ClickyStuff/Request/index.js
+++ react-app/src/components/Profile/ClickyStuff/Request/index.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
+import DatePicker from 'react-datepicker';
 
-import './Request.css'
+import 'react-datepicker/dist/react-datepicker.css';
+
+import './Request.css';
 
 const Request = () => {
+    const [date, setDate] = useState(new Date());
+
+    const handleDateChange = (date) => {
+        setDate(date);
+    };
+
     return (
         <>
             <div>Request</div>
+            <DatePicker
+                selected={date}
+                onChange={handleDateChange}
+                showTimeSelect
+                dateFormat="Pp"
+            />
         </>
     );
 };
